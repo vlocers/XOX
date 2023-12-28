2506a9bb759bb8f493d8bc35884f9c3f76eee30d
Abdullah Güzel <113038939+vlocers@users.noreply.github.com>
2023-12-28 20:34:31 +0300

    Update game-script.js

--- game-script.js
+++ game-script.js
@@ -2,12 +2,12 @@ let currentPlayer = 'X';
 let gameBoard = ['', '', '', '', '', '', '', '', ''];
 let gameActive = true;
 
-// İlk sayfadan isimleri alma
+
 const urlParams = new URLSearchParams(window.location.search);
 const player1Name = urlParams.get('player1');
 const player2Name = urlParams.get('player2');
 
-// İsimleri göster
+
 document.getElementById('status').innerText = `Sıra ${player1Name} oyuncusunda`;
 
 function makeMove(index) {
@@ -27,9 +27,9 @@ function togglePlayer() {
 
 function checkWinner() {
     const winPatterns = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-        [0, 4, 8], [2, 4, 6]             // Diagonals
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]             
     ];
 
     let isDraw = !gameBoard.includes('');
@@ -61,16 +61,16 @@ function showDrawMessage() {
 }
 
 function resetGame() {
-    // Oyunu sıfırla
+    
     currentPlayer = 'X';
     gameBoard = ['', '', '', '', '', '', '', '', ''];
     gameActive = true;
 
-    // Status div'ini göster ve içeriğini ayarla
+    
     document.getElementById('status').style.display = 'block';
     document.getElementById('status').innerText = `Sıra ${player1Name} oyuncusunda`;
 
-    // Game board'u temizle
+    
     const cells = document.getElementById('board').children;
     for (let i = 0; i < cells.length; i++) {
         cells[i].innerText = '';
